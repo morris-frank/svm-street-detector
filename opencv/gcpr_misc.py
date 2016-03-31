bc595ef673294f90fa731f230b4aea66cb1566d4
Tukkin Moriarty <mrtukkin@gmail.com>
2016-04-01 01:56:43 +0200

    Added misc py script

---
+++ opencv/gcpr_misc.py
@@ -0,0 +1,58 @@
+#!/bin/python2.7
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import scipy.io as sio
+import os
+
+
+#CONSTANTS
+BASE = '/home/morris/var/media/Elements/var/data/KITTI/'
+
+def matDirToPng(path):
+    for i in os.listdir(path):
+        if i.endswith(".mat"):
+            filename = i[:-4]
+            print filename
+            mat = sio.loadmat(path + i)
+            mat = mat['yRes'].astype(np.float32, copy=False)
+            cv2.imwrite(path + filename + '.png', 255 * mat)
+
+def addHeatMaps(idir, hdir):
+    for i in os.listdir(hdir):
+        if i.endswith(".png") and not i.endswith("_overlay.png"):
+            filename = i[:-4]
+            print filename
+            heatmap = cv2.imread(hdir + i)
+            image = cv2.imread(idir + i)
+
+            heatmap_jet = cv2.applyColorMap(heatmap, cv2.COLORMAP_SUMMER)
+
+            heatmap = heatmap/255
+
+            for c in range(0,3):
+                image[:, :, c] = np.multiply(heatmap_jet[:,:,c] , heatmap[:,:,1]) +  np.multiply(image[:,:, c] , (1.0 - heatmap[:,:,1]))
+
+            cv2.imwrite(hdir + filename + '_overlay.png', image)
+
+def main():
+    resdir = BASE + 'validation_svm/predictions/'
+
+    grabdir = BASE + 'validation_svm/grabcut/'
+    grabaodir = BASE + 'validation_svm/grabcut_ao/'
+    grabcutdir = BASE + 'validation_svm/grabcut_cut/'
+    grablccdir = BASE + 'validation_svm/grabcut_lcc/'
+
+    imdir = BASE + 'data_road/training/image/'
+
+    addHeatMaps(imdir, grabdir)
+    addHeatMaps(imdir, grabaodir)
+    addHeatMaps(imdir, grabcutdir)
+    addHeatMaps(imdir, grablccdir)
+   # matDirToPng(resdir)
+
+
+
+if __name__ == "__main__":
+    main()
